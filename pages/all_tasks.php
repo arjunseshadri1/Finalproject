da9e43784bc42b3036d5131e136e6c5907163737
rohitbhole69 <rohitbhole69@gmail.com>
2017-12-11 00:00:54 +0530

    Final Update

---
+++ pages/all_tasks.php
@@ -0,0 +1,35 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title>The HTML5 Herald</title>
+        <meta name="description" content="The HTML5 Herald">
+        <meta name="author" content="SitePoint">
+
+        <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+        <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+
+        <?php
+//this is how you print something
+
+        if ($data != NULL) {
+            print utility\htmlTable::genarateTableFromMultiArray($data);
+        }else
+        {
+            echo '<h3><a href="index.php?page=accounts&action=logout">Logout</a></h3>';
+        }
+        ?>
+
+        <a href="index.php?page=tasks&action=create">Create Todo</a><br>
+        
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
