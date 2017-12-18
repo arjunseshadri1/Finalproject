aacd2ef6bef6cc4c389e68927aa35597cced770d
Arjun Seshadrinathan <as3395@afsconnect1.njit.edu>
2017-12-18 10:50:39 -0500

    rm

--- README.md
+++
@@ -1,5 +0,0 @@
-# Finalproject
-<<<<<<< HEAD
-=======
-# FinalCode
->>>>>>> ff14d3fe6c74451461f1997bdb3284ed7e86cbdc
--- pages/delete_todo.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$task = new todo();
-$task->id = $_GET['id'];
-$task->delete();
-header('Location: index.php?page=tasks&action=all');
